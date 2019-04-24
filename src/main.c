1414bbf958738667bed4578af1bf376cdde9861d
Thales Menezes <thalesmenezes13@gmail.com>
2019-04-24 10:18:55 -0300

    Skel build start

--- src/main.c
+++ src/main.c
@@ -2,9 +2,29 @@
 #include <stdint.h>
 
 #define MEM_SIZE 4096
+#define print printf
 
 int32_t mem[MEM_SIZE];
 
+void dump_mem(uint32_t addr, uint32_t wsize);
+
 int main(){
+    print("0x%x", 256);
     return 0;
 }
+
+void dump_mem(uint32_t addr, uint32_t wsize) {
+    /**
+     * Print \a{wsize} word' content as hex,
+     * beginning from \a{addr}
+     */
+    // int32_t konstante = 0;
+    while(wsize--)
+        print("0x%x", 0); // print
+        // { print("0x%x", lw(addr,konstante)); konstante += 4; }
+}
+
+/* int32_t lw(uint32_t address, int32_t kte){
+    // if address not alligned throw Exception
+    return mem[] 
+} */
